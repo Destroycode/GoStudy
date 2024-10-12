b48e50a43cfde4e4c521c3c435bdd1fd0f12dc91
destroysus <90268610+destroysus@users.noreply.github.com>
2024-10-12 14:19:13 +0500

    start
    create package main and keyboard

---
+++ src/hi/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"program/src/keyboard"
+)
+
+func main() {
+	fmt.Println("Проверка Git!")
+	fmt.Print("Напишите градусы в Фаренгейтах: ")
+	fareng, err := keyboard.Getfloat()
+	if err != nil {
+		log.Fatal()
+	}
+	celsii := (fareng - 32) * 5 / 9
+	fmt.Printf("%0.1f градуса по Фаренгейту это %0.1f по Цельсию ", fareng, celsii)
+
+	/*
+		fmt.Println("Укажите ваши баллы: ")
+
+		grade, err := keyboard.Getfloat()
+		if err != nil {
+			log.Fatal()
+		}
+
+		var status string
+		if grade >= 60 {
+			status = "Сдал."
+		} else {
+			status = "Не сдал."
+		}
+		fmt.Println("Твои баллы:", grade, "Ты", status)
+	*/
+}
